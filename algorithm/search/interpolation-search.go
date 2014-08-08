d4d739d24a465e4d888269ceb08e9f83ac109e0c
Kirill Shvakov <shvakov@gmail.com>
2014-08-08 10:30:25 +0400

    check slice len

--- algorithm/search/interpolation-search.go
+++ algorithm/search/interpolation-search.go
@@ -2,6 +2,11 @@ package search
 
 func InterpolationSearch(needle int, haystack []int) int {
 
+	if len(haystack) == 0 {
+
+		return -1
+	}
+
 	low := 0
 	mid := 0
 	high := len(haystack) - 1
--- algorithm/search/test.go
+++ algorithm/search/test.go
@@ -7,7 +7,7 @@ import (
 
 func Test(f func(needle int, haystack []int) int, t *testing.T) {
 
-	haystack := []int{2, 5, 7, 9, 20, 21, 42}
+	haystack := []int{2, 5, 7, 9, 9, 18, 20, 21, 25, 27, 35, 42}
 
 	if val := f(9, haystack); val == -1 {
 
